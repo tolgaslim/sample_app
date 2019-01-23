b221c4d887516d6dfebf7cc08ac12d3761a4dfd7
TOLGA <tolgaefe1907@hotmail.com>
2019-01-23 17:34:57 +0300

    Root=home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   get 'static_pages/home'
   get 'static_pages/help'
+  root 'static_pages#home'
+  get 'static_pages/help'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
