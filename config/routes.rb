a231a8b8f0d0c03ece34deb49afeb21e2bfebacc
TOLGA <tolgaefe1907@hotmail.com>
2019-01-28 17:31:06 +0300

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
+  get  '/home',    to: 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
